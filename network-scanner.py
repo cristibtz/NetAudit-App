da928f6136ff07c0fdcc4c96f5451a446be31e1d
Cristian Branet <cristix231@gmail.com>
2024-10-22 16:50:39 +0300

    local network scanning functionality

--- network-scanner.py
+++ network-scanner.py
@@ -1,14 +1,59 @@
 import sys
 import scapy.all as scapy
+import socket
 
-network = sys.argv[1]
+#Currently only scans internal network
+def scan_network(netowrk):
 
-arp_frame = scapy.ARP(pdst = network)
+    print()
+    print("Sending ARP requests...")
 
-broadcast_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    arp_frame = scapy.ARP(pdst = network)
 
-broadcast_arp_frame = broadcast_arp_frame / arp_frame
+    broadcast_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
 
-replies = arp_frame.srp(broadcast_arp_frame, timeout = 1, verbose = False)[0]
+    broadcast_arp_frame = broadcast_frame / arp_frame
 
-print(replies)
+    replies = scapy.srp(broadcast_arp_frame, timeout = 5, verbose = False)[0]
+
+    hosts = []
+
+    for i in range(0, len(replies)):
+        ip = replies[i][1].psrc
+        mac = replies[i][1].hwsrc
+        try:
+            hostname = socket.gethostbyaddr(ip)[0]
+        except socket.herror:
+            hostname = "Unknown"
+        host = { 
+                "ip": ip,
+                "mac": mac.upper(),
+                "hostname": hostname
+                }
+        hosts.append(host)
+
+    return hosts
+
+def display_hosts(hosts):
+    no_of_hosts = len(hosts)
+    print()
+    print(f"There are probably {no_of_hosts} active hosts.")
+    print()
+    print("------------------------ HOSTS ------------------------")
+    print()
+
+    for i in range(0, no_of_hosts):
+        print(f"{i}. " + "IP ADDRESS: " + hosts[i]["ip"] + " | " + "MAC ADDRESS: " + hosts[i]["mac"] + " | " + \
+              "HOSTNAME: " + hosts[i]["hostname"]
+              )
+    
+    print()
+    print("-------------------------------------------------------")
+
+if __name__ == "__main__":
+
+    network = sys.argv[1]
+
+    hosts = scan_network(network)
+
+    display_hosts(hosts)
