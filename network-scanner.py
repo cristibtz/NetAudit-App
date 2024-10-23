e380dc63612d0a0137c34c1a7c2fa03cf656895d
Cristian Branet <cristix231@gmail.com>
2024-10-23 18:09:28 +0300

    Made the script run faster using threading

--- network-scanner.py
+++ network-scanner.py
@@ -1,41 +1,54 @@
-import sys
 import scapy.all as scapy
-import socket
+from queue import Queue
+import sys, socket, ipaddress, threading, time
 
 #Currently only scans internal network
-def scan_network(netowrk):
 
-    print()
-    print("Sending ARP requests...")
+print()
+print("Sending ARP requests...")
+print("Found hosts:")
+print()
 
-    arp_frame = scapy.ARP(pdst = network)
+hosts = []
+queue = Queue()
 
-    broadcast_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+def check_host():
+    while not queue.empty():
 
-    broadcast_arp_frame = broadcast_frame / arp_frame
+        ip = queue.get()    
 
-    replies = scapy.srp(broadcast_arp_frame, timeout = 5, verbose = False)[0]
+        arp_frame = scapy.ARP(pdst = ip)
 
-    hosts = []
+        broadcast_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
 
-    for i in range(0, len(replies)):
-        ip = replies[i][1].psrc
-        mac = replies[i][1].hwsrc
-        try:
-            hostname = socket.gethostbyaddr(ip)[0]
-        except socket.herror:
-            hostname = "Unknown"
-        host = { 
-                "ip": ip,
-                "mac": mac.upper(),
-                "hostname": hostname
-                }
-        hosts.append(host)
+        broadcast_arp_frame = broadcast_frame / arp_frame
 
-    return hosts
+        reply = scapy.srp1(broadcast_arp_frame, timeout = 5, verbose = False)
+
+        if reply:
+
+            mac = reply.hwsrc
+            try:
+                hostname = socket.gethostbyaddr(ip)[0]
+            except socket.herror:
+                hostname = "Unknown"
+            host = { 
+                    "ip": ip,
+                    "mac": mac.upper(),
+                    "hostname": hostname
+                    }
+            hosts.append(host)
+            print("-- " + "IP ADDRESS: " + host["ip"] + " | " + "MAC ADDRESS: " + host["mac"] + " | " + \
+              "HOSTNAME: " + host["hostname"] + " --"
+              )
+        queue.task_done()
 
 def display_hosts(hosts):
     no_of_hosts = len(hosts)
+    if no_of_hosts == 0:
+        print("No hosts found.")
+        exit()
+    
     print()
     print(f"There are probably {no_of_hosts} active hosts.")
     print()
@@ -49,11 +62,34 @@ def display_hosts(hosts):
     
     print()
     print("-------------------------------------------------------")
+    print()
 
 if __name__ == "__main__":
-
     network = sys.argv[1]
+    ip_list = [str(ip) for ip in ipaddress.IPv4Network(network, strict=False)]
 
-    hosts = scan_network(network)
+    for ip in ip_list:
+        queue.put(ip)
 
+    num_threads = 50
+    threads = []
+    
+    try:
+        for _ in range(num_threads):
+            thread = threading.Thread(target = check_host)
+            thread.daemon = True
+            threads.append(thread)
+
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join(1)
+    except KeyboardInterrupt:
+        print("Exiting...")        
+          
+    
+    print()
+    print("SCAN COMPLETE!")
     display_hosts(hosts)
+
