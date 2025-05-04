77e854a821516da4e865c871aa04a1d2ab21b0bf
⁠Cristian Gabriel Branet <gbranet@dvloper.io>
2025-05-04 12:43:24 +0000

    Misc stuff

--- scripts/web-fuzzer.py
+++ scripts/web-fuzzer.py
@@ -11,38 +11,39 @@ parser.add_argument("-u", "--url", required=True, help="URL --> example: http(s)
 parser.add_argument("-w", "--wordlist", required=True, help="HTML Tag --> example: /path/to/wordlist.txt")
 parser.add_argument("-t", "--threads", type=int, default=10, help="Number of threads to use")
 parser.add_argument("-e", "--extension", default="", help="File extension to append to each word in the wordlist (e.g., .php, .html). Only one extension")
+parser.add_argument("--web-script", action="store_true", help="Run in web script mode (no progress updates)")
 user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.6788.76 Safari/537.36"
 
 def check_url(url):
-    path = url.split("://")[1].split("/")[1]
     try:
         response = requests.get(url, timeout=5, headers={"User-Agent": user_agent}, verify=False)
-        return response.status_code, path
+        return response.status_code, url
     except Exception as e:
         print(f"Error: {e}")
         return None, None
 
 
 def fuzzer(queue, url, lock, progress, total_words):
-
     while not queue.empty():
         word = queue.get()
         full_url = f"{url}/{word}"
         try:
+            response_code, _ = check_url(full_url)
             with lock:
                 progress[0] += 1
-                response_code, path = check_url(full_url)
-                if response_code != 404:
-                    print(f"Found: /{path} - Status Code: {response_code}")
+                if response_code and response_code != 404:
+                    print(f"Found: {full_url} - Status Code: {response_code}")
         except Exception as e:
             print(f"Error: {e}")
+        finally:
+            queue.task_done()
 
 def show_progress(progress, total_words):
-    while True:
+    while progress[0] < total_words:
         input()
         print(f"Progress: {progress[0]}/{total_words}")
 
-def run_scanner(url, wordlist, threads, extension=None):
+def run_scanner(url, wordlist, threads, extension=None, web_script=False):
     queue = Queue()
     lock = threading.Lock()
     progress = [0]
@@ -62,21 +63,23 @@ def run_scanner(url, wordlist, threads, extension=None):
                     queue.put(line.strip())
                     total_words += 1
 
-    progress_thread = threading.Thread(target=show_progress, args=(progress, total_words), daemon=True)
-    progress_thread.start()
+    if not web_script:
+        progress_thread = threading.Thread(target=show_progress, args=(progress, total_words))
+        progress_thread.start()
 
     for t in range(threads):
         thread = threading.Thread(target=fuzzer, kwargs={"queue": queue, "url": url, "lock": lock, "progress": progress, "total_words": total_words}) 
-        thread.daemon = True
         thread_list.append(thread)
 
     for thread in thread_list:
         thread.start()
 
-    queue.join()
-
+    queue.join()  
     for thread in thread_list:
         thread.join()
+
+    if not web_script:
+        progress_thread.join()
 
     print("Fuzz completed!")
 
@@ -86,13 +89,12 @@ if __name__ == "__main__":
     wordlist = args.wordlist
     threads = args.threads
     extension = args.extension
+    web_script = args.web_script  
     
     print("Starting Web Fuzzer...")
     print(f"Fuzzing URL: {url}")
     print(f"Wordlist: {wordlist}")
     print(f"Date: {time.strftime('%Y-%m-%d %H:%M:%S')}")
-    print("")
-    print("Press any key to show progress...")
     print("-" * 64)
-    run_scanner(url, wordlist, threads, extension)
     
+    run_scanner(url, wordlist, threads, extension, web_script)
